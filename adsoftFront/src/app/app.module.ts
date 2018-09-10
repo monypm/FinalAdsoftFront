766d6620bdbc8c8154a192040e05cab108167225
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-09-10 16:40:49 -0500

    Created login and login-page components

--- adsoftFront/src/app/app.module.ts
+++ adsoftFront/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
