f58fbc0b1bb511f9102d46917930a82a883dd25f
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-09-11 10:59:28 -0500

    Created retrieve_profile branch with components

--- adsoftFront/src/app/app.module.ts
+++ adsoftFront/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RetrieveProfileComponent } from './retrieve-profile/retrieve-profile.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RetrieveProfileComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule
---
+++ adsoftFront/src/app/retrieve-profile/retrieve-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retrieve-profile',
+  templateUrl: './retrieve-profile.component.html',
+  styleUrls: ['./retrieve-profile.component.css']
+})
+export class RetrieveProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
