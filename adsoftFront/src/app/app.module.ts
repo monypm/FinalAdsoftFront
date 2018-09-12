ea75ecc4dbed24ccba3a9f8f593e186a9c37d2b4
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-09-12 00:31:57 -0500

    Added Landing page with button that links to login

--- adsoftFront/src/app/app.module.ts
+++ adsoftFront/src/app/app.module.ts
@@ -4,22 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginComponent } from './login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModuleModule} from './material-module/material-module.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModuleModule } from './material-module/material-module.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ErrorHandlerService } from './services/error-handler.service';
+import { AuthService } from './services/auth.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    LoginComponent
+    LoginComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModuleModule
+    MaterialModuleModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ErrorHandlerService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- adsoftFront/src/app/login-page/login-page.component.ts
+++ adsoftFront/src/app/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { ErrorHandlerService } from '../services/error-handler.service';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  id: string;
+  password: string;
 
-  constructor() { }
+  constructor(private errorHandler:ErrorHandlerService, private auth:AuthService, private router:Router) { }
 
   ngOnInit() {
+    if(this.auth.isLoggedIn()){
+      this.router.navigate(['home'])
+    }
+
+    this.id = '';
+    this.password = '';
   }
 
+  login(){
+    if(this.validate()){
+      this.auth.login(this.id, this.password)
+      .subscribe(
+        res => {
+          this.auth.setSession(res);
+          this.router.navigate(['projects']);
+        },
+        err => {
+          this.errorHandler.handleError(err);
+          this.password = '';
+        }
+      );
+    }
+    
+    return false;
+  }
+
+  validate(){
+    if(!this.id || !this.password){
+      this.errorHandler.showErrorMessage('Debes introducir tu matrícula y contraseña.');
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
 }
